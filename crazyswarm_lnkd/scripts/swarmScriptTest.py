0575be09c23af1436fc29540fab81da387a758b2
ralphtoonsutd <94339224+ralphtoonsutd@users.noreply.github.com>
2022-01-28 09:30:36 +0800

    Updated ignore and doing swarm testing

--- crazyswarm_lnkd/scripts/swarmScriptTest.py
+++ crazyswarm_lnkd/scripts/swarmScriptTest.py
@@ -32,23 +32,27 @@ if __name__ == "__main__":
 
     allcfs.takeoff(targetHeight=0.4, duration=2)
     timeHelper.sleep(2.5)
+    allcfs.goTo([0, 0.4, 0], 0, 1.0)
+    timeHelper.sleep(1.5)
 
     # Wherever the CFs take off from, move them into their correct default positions
+    """
     for cf in allcfs.crazyflies:
         pos = np.array(cf.initialPosition) + np.array([0, 0, 0.5])
         cf.goTo(pos, 0, 2.0)
     timeHelper.sleep(2.5)
-
+    """
     # Do the circle movement and break on ctrl+C
+    """
     try:
         while True:
             for count in range(0, 8, 1):
-                allcfs.crazyflies[0].goTo(circleIsh[count], 0, 1)
-                allcfs.crazyflies[1].goTo(verticalCircleIsh[count], 0, 1)
+                allcfs.goTo(circleIsh[count], 0, 1)
+                #allcfs.crazyflies[1].goTo(verticalCircleIsh[count], 0, 1)
                 timeHelper.sleep(0.5)
     except KeyboardInterrupt:
         pass
-
+    """
     input("\nPress any key to land...")
 
     allcfs.land(targetHeight=0.04, duration=2.5)
