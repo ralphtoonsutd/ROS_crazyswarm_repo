0d222a776ed2ed635eb83cd9e5c07ff8f720a280
ralphtoonsutd <94339224+ralphtoonsutd@users.noreply.github.com>
2022-03-08 17:52:42 +0800

    Added functions for circle and line formation

--- crazyswarm_lnkd/scripts/swarmScriptTest.py
+++ crazyswarm_lnkd/scripts/swarmScriptTest.py
@@ -25,13 +25,57 @@ def initSwarm():
     return swarm, timeHelper
 
 
+def circleArrangement(swarm: Crazyswarm, centreCoord):
+    # Takes centre co-ordinates and moves the swarm into a circle around them
+
+    for num, cf in enumerate(swarm.allcfs.crazyflies):
+        # 1. Create drone co-ord from its formation position and the formation's centre
+        formationPos = [0.0, 0.0, 0.0]
+        for count, initialPos in enumerate(cf.initialPosition):
+            formationPos[count] = centreCoord[count] + initialPos[count]
+
+        # 2. Split drones into 5 different movement planes
+        height = calcMovePlaneValue(num, 5)
+        cf.goTo([0, 0, height], 0, 2.5, relative=True)
+
+        # 3. Move to position and return to previous height
+        cf.goTo(formationPos, 0, 2.5, relative=False)
+        cf.goTo([0, 0, -height], 0, 2.5, relative=True)
+
+
+def lineArrangement(swarm: Crazyswarm, startCoord):
+    # Takes co-ord for start of the line and moves CFs into formation behind
+    for num, cf in enumerate(swarm.allcfs.crazyflies):
+        # 1. Create drone co-ord in the line
+        formationPos = startCoord + [num*-0.25, 0, 0]
+
+        # 2. Split drones into 5 different movement planes
+        height = calcMovePlaneValue(num, 5)
+        cf.goTo([0, 0, height], 0, 2.5, relative=True)
+
+        # 3. Move to position and return to previous height
+        cf.goTo(formationPos, 0, 2.5, relative=False)
+        cf.goTo([0, 0, -height], 0, 2.5, relative=True)
+
+
+def calcMovePlaneValue(droneNum, numPlanes):
+    # This might break for number of planes different from 5 idfk
+    height = (0.25*(numPlanes % droneNum)) - 0.5
+    return height
+
+
 if __name__ == "__main__":
 
     swarm, timeHelper = initSwarm()
     allcfs = swarm.allcfs
 
+<<<<<<< Updated upstream
     allcfs.takeoff(targetHeight=0.4, duration=2)
     timeHelper.sleep(4)
+=======
+    allcfs.takeoff(targetHeight=1.0, duration=2)
+    timeHelper.sleep(2.5)
+>>>>>>> Stashed changes
     allcfs.goTo([0, 0.4, 0], 0, 1.0)  # left
     timeHelper.sleep(1.5)
     allcfs.goTo([0.4, 0, 0], 0, 1.0)  # forward
