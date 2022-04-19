7442abc1af0b96e467da1ac092619a7de2a90ad1
ralphtoonsutd <94339224+ralphtoonsutd@users.noreply.github.com>
2022-04-19 10:57:03 +0800

    Idk lmao

--- crazyswarm_lnkd/scripts/hello_world.py
+++ crazyswarm_lnkd/scripts/hello_world.py
@@ -14,6 +14,18 @@ def main():
 
     cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+
+    cf.setParam("motion/disable", 1)
+
+    cf.goTo([0.0, 0.0, 1.5], 0, 2.5)
+    timeHelper.sleep(2.8)
+
+    cf.goTo([5.0, 0, 0], 0, 3.5)
+    timeHelper.sleep(5.0)
+
+    cf.goTo([0.0, 0.0, -1.2], 0, 3.0)
+    timeHelper.sleep(3.5)
+
     input("enter to end")
     cf.land(targetHeight=0.04, duration=2.5)
     timeHelper.sleep(TAKEOFF_DURATION)
